ad613578aae4a3b15a8df89024deea434fa22f24
AntoineChatry <antoine.chatry78@orange.fr>
2022-06-14 16:09:30 +0200

    New style

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -17,16 +17,5 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
