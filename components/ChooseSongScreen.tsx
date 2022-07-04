19a2bc0dbbe4309efac447c9d70a23b74779ef38
AntoineChatry <antoine.chatry78@orange.fr>
2022-07-04 11:39:07 +0200

    V0.02

--- components/ChooseSongScreen.tsx
+++ components/ChooseSongScreen.tsx
@@ -1,46 +1,95 @@
-import React from "react";
-import { SafeAreaView, FlatList, Text, StyleSheet, View, Image} from "react-native";
-import { Item } from "react-native-paper/lib/typescript/components/List/List";
+import React from 'react';
+import { SafeAreaView, FlatList, StyleSheet, View, Image } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 const data = [
-    {
-        "id": "1",
-        "title": "The Best Song",
-        "image": "https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg"
-    },
-    {
-        "id": "2",
-        "title": "The 2nd Best Song",
-        "image": "https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg"
-    },
-    {
-        "id": "3",
-        "title": "The 3rd Best Song",
-        "image": "https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg"
-    },
-    {
-        "id": "4",
-        "title": "The 4th Best Song",
-        "image": "https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg"
-    },
-
-]
+  {
+    id: '1',
+    title: 'Luv u',
+    image:
+      'https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg',
+    artist: 'Nekfeu',
+  },
+  {
+    id: '2',
+    title: 'The 2nd Best Song',
+    image:
+      'https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg',
+    artist: 'Nekfeu',
+  },
+  {
+    id: '3',
+    title: 'The 3rd Best Song',
+    image:
+      'https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg',
+    artist: 'Alpha Wann',
+  },
+  {
+    id: '4',
+    title: 'The 4th Best Song',
+    image:
+      'https://cdns-images.dzcdn.net/images/cover/fef4ff195f9a0966324056ed20c87823/264x264.jpg',
+    artist: 'Alpha Wann',
+  },
+];
+// add nav
 
 export default class App extends React.Component {
-    renderItem = ({item}) => (
-        <View style={{flex: 1, marginBottom: 20, marginHorizontal: 20}}>
-            <Image source={{uri: item.image}} style={{width: '100%', height: 75}} />
-            <Text style={{textAlign: "center", marginTop: 8}}>{item.title}</Text>
-        </View>
-);
+  renderItem = ({ item }) => (
+    <View style={style.container}>
+      <Image
+        source={{ uri: item.image }}
+        style={{ width: '100%', height: 133 }}
+      />
+      <Button style={style.btn}>
+        <Text style={{ marginTop: 8, fontSize: 12}} variant="labelMedium">
+          {item.title} {item.artist}
+        </Text>
+      </Button>
+    </View>
+  );
 
-
-render() {
+  render() {
     return (
-        <SafeAreaView style={{flex: 1}}>
-            <FlatList data={data} renderItem={this.renderItem} keyExtractor={item=>item.id} numColumns ={2} style={{flex: 1}} contentContainerStyle={{paddingVertical: 20}} />
-        </SafeAreaView>
+      <SafeAreaView style={style.area}>
+        <FlatList
+          data={data}
+          renderItem={this.renderItem}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          style={style.list}
+        />
+      </SafeAreaView>
     );
+  }
 }
-
-    }
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+    marginHorizontal: 20,
+    
+  },
+  area: {
+    marginTop: 105,
+    textAlign: 'center',
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    paddingVertical: 20,
+  },
+  btn: {
+    minWidth: '110%',
+    borderRadius: 25,
+    
+    height:50,
+    // alignItems:"center",
+    // justifyContent:"center",
+    marginTop: 8,
+    backgroundColor: '#96527A',
+  },
+});
