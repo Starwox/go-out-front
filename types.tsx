468f8b78bd2fdf5e943a8438e2ce2931b0d01c2a
AntoineChatry <antoine.chatry78@orange.fr>
2022-07-06 19:38:37 +0200

    Add new types

--- types.tsx
+++ types.tsx
@@ -20,6 +20,7 @@ export type RootStackParamList = {
   Code: undefined;
   Two : undefined;
   Register: undefined;
+  Song: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
@@ -32,6 +33,7 @@ export type RootTabParamList = {
   TabTwo: undefined;
   Code: undefined;
   Register: undefined;
+  Song: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
